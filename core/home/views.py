55c6d1764a4908a2dacd7b0d8669d98809a19958
aravindV14 <aravindvomula14@gmail.com>
2024-04-19 18:52:29 +0530

    usual commit

--- core/home/views.py
+++ core/home/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+
+
+def home(request):
+    return render(request,"index.html")
+
+def success_page(request):
+    return HttpResponse("<h1>this is a sucess page </h1>")
