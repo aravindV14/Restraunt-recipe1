8b1416067db244f0ab871d629f345e93e2c3fc6f
aravindV14 <aravindvomula14@gmail.com>
2024-04-20 12:49:35 +0530

    added some html

--- core/core/urls.py
+++ core/core/urls.py
@@ -19,7 +19,9 @@ from django.urls import path
 from home.views import *
 
 urlpatterns = [
-    path('',home,name="home"),
+    path('home/',home,name="home"), 
     path('success-page/',success_page,name="success_page"),
     path('admin/', admin.site.urls),
+    path('about/',about,name="about"),
+    path('contact/',contact,name="contact")
 ]
--- core/home/views.py
+++ core/home/views.py
@@ -5,7 +5,23 @@ from django.http import HttpResponse
 
 
 def home(request):
-    return render(request, 'home/index.html')
+
+    peoples =[
+        {"name":"aravind","age":21},
+        {"name":"abhiram","age":16},
+        {"name":"shiva","age":22},
+        {"name":"varun",'age':23},
+        {"name":"sharath","age":25},
+    ]
+    vegetables =["pumpkin",'tomato',"potato"]
+    return render(request, 'home/index.html',context={"peoples":peoples,"vegetables":vegetables,"page":"django"}) 
 
 def success_page(request):
     return HttpResponse("<h1>this is a sucess page </h1>")
+
+def about(request):
+
+    return render(request ,'home/about.html',context={"page":'about'})
+
+def contact(request):
+    return render(request ,'home/contact.html',context={"page":'contact'})
