d0bc6dbe4e3cde596842ebc2c415331e687f6c75
aravindV14 <aravindvomula14@gmail.com>
2024-05-15 10:35:34 +0530

    pre authentication part completed

--- core/vege/views.py
+++ core/vege/views.py
@@ -12,6 +12,8 @@ def receipes(request):
         return redirect('/receipes/')
     
     queryset =Receipe.objects.all()
+    if request.GET.get('search'):
+        queryset =queryset.filter(receipe_name__icontains =request.GET.get('search'))
     context ={'receipes':queryset}
     return render(request,'receipes.html',context)
 
@@ -21,5 +23,19 @@ def delete_receipe(request,id):
     return redirect('/receipes/')
 
 def update_receipe(request,id):
+    queryset =Receipe.objects.get(id=id)
+    if request.method =="POST":
+        data =request.POST
+        receipe_name =data.get('receipe_name')
+        receipe_description =data.get('receipe_description')
+        receipe_image =request.FILES.get('receipe_image')
 
-    return redirect('/recepies/')
+        queryset.receipe_name = receipe_name
+        queryset.receipe_description =receipe_description
+        if receipe_image:
+            queryset.receipe_image =receipe_image
+
+        queryset.save()
+        return redirect('/receipes/')
+    context ={'receipe':queryset}
+    return render(request,'update_receipes.html',context)
